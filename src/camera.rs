629d5657d991bc74c2d163a833eb0b3435032723
Sean Siddens <seansiddens@gmail.com>
2022-08-13 19:13:50 -0700

    Basic camera module

---
+++ src/camera.rs
@@ -0,0 +1,43 @@
+use crate::ray::*;
+use glam::*;
+use std::f32::consts::PI;
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new(look_from: Vec3, look_at: Vec3, v_up: Vec3, vfov: f32, aspect_ratio: f32) -> Self {
+        // From https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera
+        let theta = vfov * PI / 180.0;
+        let h = f32::tan(theta / 2.0);
+        let viewport_height = 2.0 * h;
+        let viewport_width = aspect_ratio * viewport_height;
+
+        let w = (look_from - look_at).normalize();
+        let u = v_up.cross(w).normalize();
+        let v = w.cross(u);
+
+        let origin = look_from;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, s: f32, t: f32) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
+        )
+    }
+}
