52e4122a32d71b0537950069845961724fa0b793
Sean Siddens <seansiddens@gmail.com>
2022-12-29 14:49:27 -0800

    Using Affine3A instead of Mat4

--- src/mesh.rs
+++ src/mesh.rs
@@ -4,7 +4,6 @@ use glam::*;
 use std::error::Error;
 
 // TODO: Handle other data (normals, UV, materials, etc).
-// TODO: Use Affine3A instead of Mat4?
 
 /// For now, a Mesh is just a vector of Triangles.
 #[derive(Debug)]
@@ -18,8 +17,8 @@ pub struct Mesh {
     rotation: Quat,
     translation: Vec3A,
 
-    model_to_world: Mat4,
-    world_to_model: Mat4,
+    model_to_world: Affine3A,
+    world_to_model: Affine3A,
 }
 
 #[allow(dead_code)]
@@ -57,8 +56,8 @@ impl Mesh {
             scale: Vec3A::ONE,
             rotation: Quat::IDENTITY,
             translation: Vec3A::ZERO,
-            model_to_world: Mat4::IDENTITY,
-            world_to_model: Mat4::IDENTITY,
+            model_to_world: Affine3A::IDENTITY,
+            world_to_model: Affine3A::IDENTITY,
         })
     }
 
@@ -70,8 +69,8 @@ impl Mesh {
             scale: Vec3A::ONE,
             rotation: Quat::IDENTITY,
             translation: Vec3A::ZERO,
-            model_to_world: Mat4::IDENTITY,
-            world_to_model: Mat4::IDENTITY,
+            model_to_world: Affine3A::IDENTITY,
+            world_to_model: Affine3A::IDENTITY,
         }
     }
 
@@ -83,7 +82,7 @@ impl Mesh {
     pub fn translation(&mut self, translation: Vec3A) {
         // Update transform.
         self.translation = translation;
-        self.model_to_world = Mat4::from_scale_rotation_translation(
+        self.model_to_world = Affine3A::from_scale_rotation_translation(
             self.scale.into(),
             self.rotation.into(),
             self.translation.into(),
@@ -95,7 +94,7 @@ impl Mesh {
         self.scale = scale;
         self.rotation = rotation;
         self.translation = translation;
-        self.model_to_world = Mat4::from_scale_rotation_translation(
+        self.model_to_world = Affine3A::from_scale_rotation_translation(
             scale.into(),
             rotation.into(),
             translation.into(),
@@ -108,26 +107,17 @@ impl Hittable for Mesh {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitPayload) -> bool {
         // Transform the ray to model space.
         let ray = Ray::new(
+            self.world_to_model.transform_point3a(r.origin()),
             self.world_to_model
-                .transform_point3(r.origin().into())
-                .into(),
-            self.world_to_model
-                .transform_vector3(r.direction().normalize().into())
-                .into(),
+                .transform_vector3a(r.direction().normalize()),
         );
 
         let use_bvh = true;
         if use_bvh {
             let hit_anything = if self.bvh.hit(&ray, t_min, t_max, rec) {
                 // Transform the hit position and hit surface normal back to world space.
-                rec.world_position = self
-                    .model_to_world
-                    .transform_point3(rec.world_position.into())
-                    .into();
-                rec.world_normal = self
-                    .model_to_world
-                    .transform_vector3(rec.world_normal.into())
-                    .into();
+                rec.world_position = self.model_to_world.transform_point3a(rec.world_position);
+                rec.world_normal = self.model_to_world.transform_vector3a(rec.world_normal);
 
                 true
             } else {
@@ -152,8 +142,8 @@ impl Hittable for Mesh {
                 }
             }
             // Transform the hit position and hit surface normal back to world space.
-            rec.world_position = self.model_to_world.transform_point3(rec.world_position.into()).into();
-            rec.world_normal = self.model_to_world.transform_vector3(rec.world_normal.into()).into();
+            rec.world_position = self.model_to_world.transform_point3a(rec.world_position);
+            rec.world_normal = self.model_to_world.transform_vector3a(rec.world_normal);
 
             hit_anything
         }
