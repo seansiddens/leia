f0637f7521d789bbd80e1b202744aaa3d746f8e4
Sean Siddens <seansiddens@gmail.com>
2022-12-29 12:32:55 -0800

    Refactored to match a ray tracing pipelien

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,45 +1,9 @@
 use crate::{
-    hittable::{HitRecord, Hittable},
+    hittable::{HitPayload, Hittable},
     hittable_list::HittableList,
     Camera, Color, Ray,
 };
 use glam::Vec3;
-
-/// Returns the color of the surface a given ray is pointing at.
-fn ray_color(r: &Ray, world: &HittableList) -> Color {
-    // if depth <= 0 {
-    //     // Exceeded path length.
-    //     return Vec3::ZERO;
-    // }
-
-    // Check if ray intersects world.
-    let mut rec = HitRecord::new();
-    rec.t = f32::INFINITY;
-
-    if !world.hit(r, 0.0, f32::INFINITY, &mut rec) {
-        // If ray hits nothing, return background color.
-        let unit_dir = r.direction().normalize();
-        let t = 0.5 * (unit_dir.y + 1.0);
-
-        // Returns a color lerped between white and blu-ish
-        return (1.0 - t) * Color::ONE + t * Color::new(0.5, 0.7, 1.0);
-    }
-
-    // Ray hit something.
-    // Build ONB from normal.
-    // let uvw = Onb::from_w(rec.normal);
-    // let direction = uvw.local(cosine_sample_hemisphere(
-    //     rng.random_uniform(),
-    //     rng.random_uniform(),
-    // ));
-
-    // let scattered = Ray::new(rec.p, direction.normalize());
-
-    // // Color based on surface normal
-    return 0.5 * (rec.normal + Vec3::ONE);
-
-    // 0.8 * ray_color(&scattered, world, depth - 1, rng)
-}
 
 pub struct Renderer {
     image_data: Vec<u8>,
@@ -67,29 +31,24 @@ impl Renderer {
         }
     }
 
-    pub fn render(&mut self, scene: &HittableList, cam: &Camera) {
-        let mut view_ray = Ray::new(Vec3::ZERO, Vec3::ZERO);
-        view_ray.set_origin(*cam.get_position());
-        let ray_directions = cam.get_ray_directions();
+    /// Get reference to final image buffer.
+    pub fn get_final_image(&self) -> &Vec<u8> {
+        &self.image_data
+    }
 
+    /// Render current scene to image buffer.
+    pub fn render(&mut self, scene: &HittableList, cam: &Camera) {
         for y in 0..self.image_height {
             for x in 0..self.image_width {
-                // Index into image buffer.
-                let i = y * self.image_width * 4 + (x * 4);
 
-                // // Normalized texture coords into the image
-                // let u = (x as f32) / (self.image_width - 1) as f32;
-                // let v = 1.0 - ((y as f32) / (self.image_height - 1) as f32);
-
-                // let view_ray = cam.get_ray(u, v);
-                view_ray.set_direction(ray_directions[x + y * self.image_width]);
-
-                let col = ray_color(&view_ray, scene);
+                let col = self.per_pixel(scene, cam, x, y);
                 let r = (col.x * 255.0) as u8;
                 let g = (col.y * 255.0) as u8;
                 let b = (col.z * 255.0) as u8;
 
                 // Write colors to buffer.
+                // Index into image buffer.
+                let i = y * self.image_width * 4 + (x * 4);
                 self.image_data[i] = r;
                 self.image_data[i + 1] = g;
                 self.image_data[i + 2] = b;
@@ -97,15 +56,44 @@ impl Renderer {
         }
     }
 
-    fn per_pixel(&self, u: f32, v: f32) -> Color {
-        Color {
-            x: 1.0,
-            y: 0.5,
-            z: 0.0,
+    /// RayGen shader
+    fn per_pixel(&self, scene: &HittableList, cam: &Camera, x: usize, y: usize) -> Color {
+        // Initialize the view ray.
+        let mut view_ray = Ray::new(Vec3::ZERO, Vec3::ZERO);
+        view_ray.set_origin(*cam.get_position());
+        view_ray.set_direction(cam.get_ray_directions()[(x + y * self.image_width)]);
+
+        let hit_payload = self.trace_ray(scene, &view_ray);
+        if hit_payload.hit_distance < 0.0 {
+            // If ray hits nothing, return background color.
+            let unit_dir = view_ray.direction().normalize();
+            let t = 0.5 * (unit_dir.y + 1.0);
+
+            // Returns a color lerped between white and blu-ish
+            return (1.0 - t) * Color::ONE + t * Color::new(0.5, 0.7, 1.0);
         }
+
+        Color::ONE
     }
 
-    pub fn get_final_image(&self) -> &Vec<u8> {
-        &self.image_data
+    fn trace_ray(&self, scene: &HittableList, ray: &Ray) -> HitPayload {
+        // Check if ray intersects world.
+        let mut hit_payload = HitPayload::new();
+        hit_payload.hit_distance = f32::INFINITY;
+
+        if !scene.hit(ray, 0.0, f32::INFINITY, &mut hit_payload) {
+            // Invoke the miss function.
+            return self.miss(ray);
+        }
+
+        // Will contain hit information about the closest intersection.
+        hit_payload
+    }
+
+    /// Invoked every time a ray misses every object in the scene.
+    fn miss(&self, ray: &Ray) -> HitPayload {
+        let mut hit_payload = HitPayload::new();
+        hit_payload.hit_distance = -1.0;
+        hit_payload
     }
 }
