db793d0dda47b4da5bc2377345fa517cd7ca4c11
Sean Siddens <seansiddens@gmail.com>
2022-12-31 22:35:49 -0800

    Handling mouse input

--- src/input.rs
+++ src/input.rs
@@ -1,13 +1,21 @@
-use winit::event::{ElementState, WindowEvent, VirtualKeyCode};
+use winit::event::{ElementState, MouseButton, VirtualKeyCode, WindowEvent};
 
 pub struct InputState {
+    // Index via virtual keycode.
     key_state: [ElementState; 255],
+    // Index via MouseButton enum?
+    mouse_state: [ElementState; 3], // Left, Right, or Middle
+    mouse_position: (f32, f32),
+    last_mouse_position: Option<(f32, f32)>,
 }
 
 impl InputState {
     pub fn new() -> Self {
         Self {
             key_state: [ElementState::Released; 255],
+            mouse_state: [ElementState::Released; 3],
+            mouse_position: (0.0, 0.0),
+            last_mouse_position: None,
         }
     }
 
@@ -15,15 +23,20 @@ impl InputState {
         match event {
             WindowEvent::KeyboardInput { input, .. } => {
                 if let Some(keycode) = input.virtual_keycode {
-                    match input.state {
-                        ElementState::Pressed => {
-                            self.key_state[keycode as usize] = ElementState::Pressed;
-                        }
-                        ElementState::Released => {
-                            self.key_state[keycode as usize] = ElementState::Released;
-                        }
-                    }
+                    self.key_state[keycode as usize] = input.state;
                 }
+            }
+            WindowEvent::MouseInput { state, button, .. } => match button {
+                MouseButton::Left => self.mouse_state[0] = state,
+                MouseButton::Right => self.mouse_state[1] = state,
+                MouseButton::Middle => self.mouse_state[2] = state,
+                _ => {}
+            },
+            WindowEvent::CursorMoved { position, .. } => {
+                // Update last mouse pos.
+                self.last_mouse_position = Some(self.mouse_position);
+                // Set current mouse pos.
+                self.mouse_position = (position.x as f32, position.y as f32);
             }
             _ => {}
         }
@@ -31,5 +44,32 @@ impl InputState {
 
     pub fn is_key_down(&self, key: VirtualKeyCode) -> bool {
         self.key_state[key as usize] == ElementState::Pressed
+    }
+
+    pub fn is_mouse_button_down(&self, button: MouseButton) -> bool {
+        let state = match button {
+            MouseButton::Left => self.mouse_state[0],
+            MouseButton::Right => self.mouse_state[1],
+            MouseButton::Middle => self.mouse_state[2],
+            _ => ElementState::Released,
+        };
+
+        state == ElementState::Pressed
+    }
+
+    pub fn get_mouse_pos(&self) -> (f32, f32) {
+        self.mouse_position
+    }
+
+    /// Get the change in mouse coordinates.
+    pub fn get_mouse_delta(&self) -> (f32, f32) {
+        if let Some(last_mouse_pos) = self.last_mouse_position {
+            return (
+                self.mouse_position.0 - last_mouse_pos.0,
+                self.mouse_position.1 - last_mouse_pos.1,
+            );
+        }
+
+        (0.0, 0.0)
     }
 }
